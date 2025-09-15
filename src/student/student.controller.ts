771539f770fa850e6968f0cd15896c027cea0bc1
nayemalways <nishanahmed13913@gmail.com>
2025-09-15 14:24:26 +0600

    created crud controller and services

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,11 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { StudentService } from './student.service';
+import type { IStudent } from './student.interface';
 
 @Controller('student')
 export class StudentController {
     constructor(private readonly studentService: StudentService) {};
+
     @Get()
     getAllStudent() {
-        
+        return this.studentService.getStudents();
+    }
+
+    @Post()
+    createStudent(@Body() body:IStudent) {
+        return this.studentService.createStudent(body);
+    }
+
+    @Delete()
+    deleteStudent(@Param() id: string) {
+        console.log(id)
+        return id;
     }
 }
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,6 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { IStudent } from './student.interface';
 
 @Injectable()
 export class StudentService {
-    
+    private students: IStudent[] = [];
+
+    createStudent(data: IStudent) {
+         this.students.push(data);
+         return "Student created success!";
+    }
+
+    getStudents() {
+        return this.students;
+    }
+
+    deleteStudent(id: number) {
+        let deleteItem = this.students.filter(student => student.id != id);
+        this.students.push(...deleteItem);
+        return {
+            message: "Deleted success",
+            data: this.students
+        }
+    }
+       
 }
